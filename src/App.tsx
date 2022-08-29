40ddda44db4079dd55cbd9177edc8bcd821bf635
zhuozhenze <284555723@qq.com>
2022-08-29 13:37:52 +0800

    feat(*) modify ant-design style file

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ErrorBoundary from "./components/error-boundary";
 import Router from "./router";
 import "./App.css";
-import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
 
 function App() {
   return (
